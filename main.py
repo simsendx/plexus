d6ee1dec97ea451e6e9ce9d2087b5178d93f24d3
Stefan Filges <filges.stefan@gmail.com>
2023-08-24 16:35:07 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import pysam
+import primer3
+from Bio import SeqIO
+
+
+# Get mutation list
+
+
+# Get metadata
+min_primer_length = 18
+max_primer_lgenth = 30
+min_amplicon_size = 40
+max_amplicon_size = 80
+min_padding_bases = 3
+
+
+# Import design regions
+
+# For each mutation
+#   get sequence +/- 200 bp
+#   define design regions
+    
+
+# Use pysam to get region from fasta
+# https://pysam.readthedocs.io/en/latest/api.html
+
+
+
